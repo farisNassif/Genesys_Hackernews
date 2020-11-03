c0e3b78b2f49c98394bd85cfed6d61e1fa04a199
farisNassif <g00347032@gmit.ie>
2020-11-03 01:04:21 +0000

    More unit tests added + more code coverage

--- hackernews-frontend/src/app/story/service/story.service.spec.ts
+++ hackernews-frontend/src/app/story/service/story.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { StoryService } from './story.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 describe('StoryService', () => {
   const httpClientMock = jasmine.createSpyObj('HttpClient', ['get']);
@@ -11,8 +12,32 @@ describe('StoryService', () => {
     ]
   }));
 
+  /* Creation */
   it('should be created', () => {
     const service: StoryService = TestBed.get(StoryService);
     expect(service).toBeTruthy();
+  });
+
+  /* Make sure stories are returned by type */
+  it('#getStoriesByType should get stories by type from the hackernews api', () => {
+    const service: StoryService = TestBed.get(StoryService);
+    httpClientMock.get.and.returnValue(new Observable((o) => {
+      o.next([123]); 
+    }));
+    service.getStoriesByType('new').then((data) => {
+      expect(data).toEqual([123]);
+      expect(service.stories).toEqual([123]);
+    });
+  });
+
+  /* Make sure a story is returned as intended */
+  it('#getStory should get a single story from the hackernews api', () => {
+    const service: StoryService = TestBed.get(StoryService);
+    httpClientMock.get.and.returnValue(new Observable((o) => { 
+      o.next('test'); 
+    }));
+    service.getStory(123).subscribe((data) => {
+      expect(data).toEqual('test');
+    });
   });
 });
--- hackernews-frontend/src/app/story/story.component.spec.ts
+++ hackernews-frontend/src/app/story/story.component.spec.ts
@@ -1,4 +1,3 @@
-/** 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StoryComponent } from './story.component';
@@ -23,8 +22,33 @@ describe('StoryComponent', () => {
     fixture.detectChanges();
   });
 
+  /* Creation */
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /* Story object / display test */
+  it('should display story details correctly', () => {
+    component.story = { id: 1, title: 'test_story', url: 'https://www.youtube.com', descendants: 1, score: 999, by: 'testuser', time: 123 };
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    /* Letting the test framework know which html elements are what */
+    const title = nativeElement.querySelector('mat-card-title').textContent;
+    const subtitle = nativeElement.querySelector('mat-card-subtitle').textContent;
+    const url = nativeElement.querySelector('mat-card-subtitle:last-child').textContent;
+    /* Expectations */
+    expect(title).toContain('test_story');
+    expect(subtitle).toContain('999 Points Submitted');
+    expect(url).toContain('youtube.com');
+  });
+
+  /* Test unix time conversion */
+  it('should correctly convert unixtime to a readable format', () => {
+    expect(component.getTimeAgo(123)).toEqual("618 months ago by ");
+    /* I would test more, but since it's time based the test would
+      fail if ran in the future, since a unixtime representing 10
+      seconds ago right now could equal to potentially 'weeks ago' 
+      at some point in the future
+    */
+  });
 });
-*/
